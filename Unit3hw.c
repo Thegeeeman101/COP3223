75964b0b2017eed4cb5fdaf5606dc4ec87112699
jasongouws <jasongouws@icloud.com>
2024-09-16 13:34:16 -0400

    Unit3hw.c

--- Unit3hw.c
+++ Unit3hw.c
@@ -5,8 +5,9 @@ int main()
 {
   int array[SIZE] = {10, 40, 30, 10, 14, 72, 81, 90, 100, 57};
   int largestValue = array[0];
-  float average;
+  float average = 0.0;
   int index = 0;
+  int sum = 0;
 
 
   while ( index < SIZE)
@@ -15,9 +16,13 @@ int main()
       {
           largestValue = array[index];
       }
+      sum += array[index];
       index++;
   }
-    printf("The largest value in the array is %d\n", largestValue);
+
+  average = (float)sum / SIZE;
+  printf("The largest value in the array is %d\n", largestValue);
+  printf("The average value of the array is %.2f\n" , average);
 
 
 }
