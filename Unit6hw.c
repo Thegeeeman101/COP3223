cd4a008cb26820c49b77d19b4976e1186fdf1ff0
davidljerome03 <davidljerome03@gmail.com>
2024-11-01 14:11:32 -0400

    made the code look nicer

--- Unit6hw.c
+++ Unit6hw.c
@@ -104,18 +104,23 @@ int main()
     char *upper_sorted;
     char most_occur = -1;
     int lower_case=0 , upper_case=0, digits=0;
+
     printf("Enter your string: ");
     gets(str);
+
     most_occur = most_occuring_character ( str );
     if ( most_occur == -1 ) printf ("All characters appears once\n");
     else printf ("The most occuring character is: %c \n", most_occur);
+
     count_low_up_digit ( str, &lower_case, &upper_case , &digits );
     printf ("There is/are %d lower case letter(s)\n", lower_case);
     printf ("There is/are %d upper case letter(s)\n", upper_case);
     printf ("There is/are %d digit(s)\n", digits);
+
     upper_sorted = Up_letters ( str );
     printf ("%s\n", upper_sorted);
     printf ("%s\n", str);
+    
     return 0;
 }
 
